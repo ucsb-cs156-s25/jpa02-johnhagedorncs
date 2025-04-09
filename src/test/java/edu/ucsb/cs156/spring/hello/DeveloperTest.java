2dbda84635b6abf865e602806338c1ce34ad3b53
Johnny Hagedorn <johnny@169-231-19-101.wireless.ucsb.edu>
2025-04-08 17:36:42 -0700

    jh - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -25,6 +25,10 @@ public class DeveloperTest {
         assertEquals("John H.", Developer.getName());
     }
 
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+        assertEquals("johnhagedorncs", Developer.getGithubId());
+    }
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
